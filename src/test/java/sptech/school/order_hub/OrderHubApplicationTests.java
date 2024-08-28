b8805bf9cf3d80bc1393beb2bf3721a782e0c214
Jonathan Carvalho <jonathan.carvalho@cxpbrasil.com.br>
2024-08-28 17:31:23 -0300

    Inicializando projeto Spring

---
+++ src/test/java/sptech/school/order_hub/OrderHubApplicationTests.java
@@ -0,0 +1,13 @@
+package sptech.school.order_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
