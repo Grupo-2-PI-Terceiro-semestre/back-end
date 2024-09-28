2af54f1914e168f133d024f5f2b608f886a0c654
Jonathan Carvalho <jonathan.carvalho@cxpbrasil.com.br>
2024-09-28 17:30:05 -0300

    Adicionando Genero

--- src/main/java/sptech/school/order_hub/entitiy/Pessoa.java
+++ src/main/java/sptech/school/order_hub/entitiy/Pessoa.java
@@ -5,7 +5,10 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
@@ -30,8 +33,10 @@ public abstract class Pessoa {
     @Email
     private String emailPessoa;
 
-
     private LocalDate dataNascimento;
+
+    private String genero;
+
     private String numeroTelefone;
 
     @ManyToOne
