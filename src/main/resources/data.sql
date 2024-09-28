ac11eee872d80a009f5ce88bd6ce405a5428c7eb
Jonathan Carvalho <jonathan.carvalho@cxpbrasil.com.br>
2024-09-28 17:27:51 -0300

    Adicionando Dados empresa

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO categoria (id_categoria, nome) VALUES (1, 'Salões de Beleza');
+INSERT INTO categoria (id_categoria, nome) VALUES (2, 'Barbearias');
+INSERT INTO categoria (id_categoria, nome) VALUES (3, 'Manicure & Nail Designer');
+INSERT INTO categoria (id_categoria, nome) VALUES (4, 'Esteticistas');
+INSERT INTO categoria (id_categoria, nome) VALUES (5, 'Sobrancelhas & Cílios');
