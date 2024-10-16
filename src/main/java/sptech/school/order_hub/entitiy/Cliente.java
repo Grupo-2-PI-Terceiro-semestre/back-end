82809b959ca85d31748c29308c5b775ea0412b3c
JonathanCarvalho39 <jonathan.carvalho@sptech.school.com>
2024-10-16 20:12:33 -0300

    Modificando entidade CLIENTE

--- src/main/java/sptech/school/order_hub/entitiy/Cliente.java
+++ src/main/java/sptech/school/order_hub/entitiy/Cliente.java
@@ -2,18 +2,20 @@ package sptech.school.order_hub.entitiy;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.*;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Builder
 @EqualsAndHashCode(callSuper = false)
 @Table(name = "CLIENTE")
 public class Cliente extends Pessoa {
 
-    public Cliente(
-    ) {
+    public Cliente() {
         super();
     }
-
 }
