26ab7a1551cb84b9dac99665ecd9bfd7546fcfbb
JonathanCarvalho39 <jonathan.carvalho@sptech.school>
2025-05-06 19:20:32 -0300

    Ajusntando para fazer o agendamento pelo app

--- src/main/java/sptech/school/order_hub/repository/AgendaRepository.java
+++ src/main/java/sptech/school/order_hub/repository/AgendaRepository.java
@@ -13,11 +13,11 @@ public interface AgendaRepository extends JpaRepository<Agenda, Integer> {
             SELECT
                 s.duracao,
                 TIME(a.data_hora) AS horaInicio,
-                TIME(DATE_ADD(a.data_hora, INTERVAL TIMESTAMPDIFF(MINUTE, '00:00:00', s.duracao) MINUTE)) AS horaFinal
+                TIME(DATE_ADD(a.data_hora, INTERVAL TIME_TO_SEC(s.duracao) SECOND)) AS horaFinal
             FROM agendamento AS a
-            JOIN servico AS s ON a.fk_servico = s.id_servico
-            JOIN agenda AS ag ON a.fk_agenda = ag.id_agenda
-            JOIN usuarios AS u ON ag.fk_usuario = u.id_pessoa
+                     JOIN servico AS s ON a.fk_servico = s.id_servico
+                     JOIN agenda AS ag ON a.fk_agenda = ag.id_agenda
+                     JOIN usuarios AS u ON ag.fk_usuario = u.id_pessoa
             WHERE
                 s.fk_empresa = ?1
                 AND ag.id_agenda = ?2
