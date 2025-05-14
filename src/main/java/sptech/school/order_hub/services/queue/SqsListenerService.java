b53ed7dcc810f432afb795a856b507568b50b885
JonathanCarvalho39 <jonathan.carvalho@sptech.school>
2025-05-14 01:49:25 -0300

    Adicionando logs

--- src/main/java/sptech/school/order_hub/services/queue/SqsListenerService.java
+++ src/main/java/sptech/school/order_hub/services/queue/SqsListenerService.java
@@ -3,9 +3,11 @@ package sptech.school.order_hub.services.queue;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.awspring.cloud.sqs.annotation.SqsListener;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class SqsListenerService {
 
@@ -20,11 +22,16 @@ public class SqsListenerService {
     @SqsListener(value = "${aws.sqs.queue.name}")
     public void consumer(String messageJson) throws JsonProcessingException {
         try {
+            log.info("##############################################");
+            log.info("Recebendo mensagem para SQS: {}", messageJson);
+            log.info("##############################################");
             ObjectMapper mapper = new ObjectMapper();
             MessageEmpresa message = mapper.readValue(messageJson, MessageEmpresa.class);
 
             String destino = "/topic/" + message.empresaId() + "/notifications";
             messagingTemplate.convertAndSend(destino, message.mensagem());
+
+            log.info("Mensagem enviada via WS");
 
         } catch (Exception e) {
             System.err.println("Erro ao processar mensagem: " + e.getMessage());
--- src/main/java/sptech/school/order_hub/services/queue/SqsProducerService.java
+++ src/main/java/sptech/school/order_hub/services/queue/SqsProducerService.java
@@ -1,12 +1,15 @@
 package sptech.school.order_hub.services.queue;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 @Service
+@Slf4j
 public class SqsProducerService {
 
     private final SqsAsyncClient sqsClient;
@@ -18,8 +21,12 @@ public class SqsProducerService {
         this.sqsClient = sqsClient;
     }
 
+
     public void send(Object mensagem) {
         try {
+            log.info("##############################################");
+            log.info("Enviando mensagem para SQS: {}", mensagem);
+            log.info("##############################################");
             sqsClient.sendMessage(SendMessageRequest.builder()
                     .queueUrl(QUEUE_URL)
                     .messageBody(new ObjectMapper().writeValueAsString(mensagem))
