4ee46736190d42149b801ca23f860f2f8d77790e
JonathanCarvalho39 <jonathan.carvalho@sptech.school>
2025-05-14 17:36:36 -0300

    Ajuste no envio da mensagem

--- src/main/java/sptech/school/order_hub/services/queue/SqsProducerService.java
+++ src/main/java/sptech/school/order_hub/services/queue/SqsProducerService.java
@@ -21,19 +21,29 @@ public class SqsProducerService {
         this.sqsClient = sqsClient;
     }
 
-
     public void send(Object mensagem) {
         try {
-            log.info("##############################################");
-            log.info("Enviando mensagem para SQS: {}", mensagem);
-            log.info("##############################################");
+
+            String json = new ObjectMapper().writeValueAsString(mensagem);
+
             sqsClient.sendMessage(SendMessageRequest.builder()
                     .queueUrl(QUEUE_URL)
-                    .messageBody(new ObjectMapper().writeValueAsString(mensagem))
-                    .build());
+                    .messageBody(json)
+                    .build()
+            ).whenComplete((response, exception) -> {
+                if (exception != null) {
+                    log.error("Erro ao enviar mensagem para SQS", exception);
+                } else {
+                    log.info("###################################################################");
+                    log.info("Mensagem enviada com sucesso. MessageId: {}", response.messageId());
+                    log.info("###################################################################");
+                }
+            });
+
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Erro ao serializar a mensagem", e);
         }
     }
+
 }
 
